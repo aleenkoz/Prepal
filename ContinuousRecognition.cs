c875ce412f103a28ea7a2a9e19bf654026b4153e
Aleen Alzahrani <aleen0617@hotmail.com>
2025-07-14 18:02:23 +0300

    Prepal
       Prepal is an AI tool that uses the powers of Natural language proccessing of the Speech tools by Microsoft Azure. You can run prepal while a slideshow is running, it will listen to the keywords: “Prepal Next” or “Prepal End” and will perform the corresponding act whether it is moving to the next slide or ending the slideshow.
    Prepal was developed using the programming language C#, to ease the proccess of using Azure Speech Services, and easily make the connection with the Microsoft PowerPoint app using built-in libraries.

---
+++ ContinuousRecognition.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Runtime.InteropServices;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Core = Microsoft.Office.Core;
+
+
+
+namespace Prepal_Back_End_
+{
+    /*This class approaches the purpose of prepal using continuos recognition.*/
+    internal class ContinuousRecognition
+    {
+        async static Task Main(string[] args)
+        {
+            var program = new LoopedRecognition();
+
+            var speechConfig = SpeechConfig.FromSubscription("key", "area");
+            var pptApp = (PowerPoint.Application)
+            Marshal.GetActiveObject("PowerPoint.Application");
+
+            //We first check if the slideshow is running and ready for prepal.
+            if (pptApp == null && pptApp.SlideShowWindows.Count == 0)
+            {
+                Console.WriteLine("No slideshow is running right now.");
+                return;
+            }
+            else
+            {
+                var slideShow = pptApp.SlideShowWindows[1].View;
+                using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+                using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+                {
+                    //In continuous recognition we check for every possible outocome. Starting with text being recognized.
+                    var stopRecognition = new TaskCompletionSource<int>();
+                    recognizer.Recognizing += (s, e) =>
+                    {
+                        string textBeingRecognized = e.Result.Text;
+                    };
+
+                    //Recognized text, where prepal will take action.
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            string recognizedText = e.Result.Text;
+                            string test = program.returnKeyword(recognizedText);
+                            if(test== "prepal next")
+                            {
+                                slideShow.Next();
+                                Console.WriteLine("Moved to the next slide.");
+                            }
+                            else if(test== "prepal end")
+                            {
+                                slideShow.Exit();
+                                Console.WriteLine("Ended the slideshow.");
+                            }
+                        }
+                        else if (e.Result.Reason == ResultReason.NoMatch)
+                        {
+                            Console.WriteLine("Speech couldn't be recognized.");
+                        }
+                    };
+                    //Cancelation of recognition for all different reasons.
+                    recognizer.Canceled += (s, e) =>
+                    {
+                        Console.WriteLine($"Canceed: Reason= {e.Reason}");
+                        if (e.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"Error Code = {e.ErrorCode}");
+                            Console.WriteLine($"Error Details = {e.ErrorDetails}");
+                        }
+
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    //When the API stops recognizing.
+                    recognizer.SessionStopped += (s, e) =>
+                    {
+                        Console.WriteLine("Session Stopped");
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    await recognizer.StartContinuousRecognitionAsync();
+                    Task.WaitAny(new[] { stopRecognition.Task });
+
+                }
+
+            }
+        }
+    }
+}
---
+++ LoopedRecognition.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Runtime.InteropServices;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Core = Microsoft.Office.Core;
+
+
+/*This class approaches the purpose of prepal using single-shot voice recognition implemented 
+ inside a while loop. */
+namespace Prepal_Back_End_
+{
+    internal class LoopedRecognition
+    {
+        /*This is an internal method to use for proccessing the recieved transcribed text
+         and find whether the anticipated keyword was said or not. */
+
+        public string returnKeyword (string resultText)
+        {
+            resultText = resultText.Trim().ToLowerInvariant();
+            string lookingFor = "prepal next";
+            string orFor = "prepal end";
+            if(resultText.IndexOf(lookingFor, StringComparison.Ordinal) != 1)
+            {
+                return resultText.Substring(resultText.IndexOf(lookingFor, StringComparison.Ordinal) + 1, lookingFor.Length);
+            }
+            else if (resultText.IndexOf(orFor, StringComparison.Ordinal) != 1)
+            {
+                return resultText.Substring(resultText.IndexOf(orFor, StringComparison.Ordinal) + 1, lookingFor.Length);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        //The main method, where the main action happens.
+        async static Task Main(string[] args)
+        {
+            var program = new LoopedRecognition();
+
+            //Create an instance of the speech configration to use the Azure API 
+            var speechConfig = SpeechConfig.FromSubscription("key", "area");
+
+            //Calling the PowerPoint application listner
+            var pptApp = (PowerPoint.Application)
+            Marshal.GetActiveObject("PowerPoint.Application");
+
+            //An if statement that checks if the listner is running and if the slideshow started, if not the application is terminated
+            if (pptApp == null && pptApp.SlideShowWindows.Count == 0)
+            {
+                Console.WriteLine("Mo slideshow is running.");
+                return;
+            }
+            var slideShow = pptApp.SlideShowWindows[1].View;
+            Console.WriteLine("Listening for the keyword...");
+
+
+            // We use the transcribtion API and the previously defined internal method to look for the moving and endinig the slideshow keywords.
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+            {
+                recognizer.Recognized += (s, e) =>
+                {
+                    string result = e.Result.Text;
+                    string NeededResult = program.returnKeyword(result);
+
+                    if (result == null || result == "")
+                    {
+                        return;
+                    }
+                    switch (result)
+                    {
+                        case "prepal next":
+                            slideShow.Next();
+                            Console.WriteLine("Moved to the net slide successfully");
+                            break;
+                        case "prepal end":
+                            slideShow.Exit();
+                            Console.WriteLine("Ended the slideshow successfully");
+                            break;
+                    }
+                };
+                await recognizer.StartContinuousRecognitionAsync();
+                await recognizer.StopContinuousRecognitionAsync();
+            }
+
+        }
+    }
+}
